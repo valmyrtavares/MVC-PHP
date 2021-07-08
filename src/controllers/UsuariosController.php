837f2d9fcd99a58e0abc5f5baf32b83344d7b504
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-07-08 13:56:42 -0300

    DATA SEND TO DB

--- src/controllers/UsuariosController.php
+++ src/controllers/UsuariosController.php
@@ -2,6 +2,7 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\models\Usuario;
 
 class UsuariosController extends Controller {
 
@@ -10,7 +11,29 @@ class UsuariosController extends Controller {
     } 
 
     public function addAction() {
-       echo 'RECEBIDO';
+       $name = filter_input(INPUT_POST, 'name');
+       $email = filter_input(INPUT_POST, 'email');
+
+       if($name && $email) {
+            $data = Usuario::select()->where('email', $email)->execute();
+
+            if(count($data)=== 0){
+                Usuario::insert([
+                    'nome' => $name,
+                    'email'=> $email
+                ])->execute();    
+                // redirect /      
+                // echo 'inseriu';
+                // exit;
+                $this->redirect(('/'));
+              
+            }
+
+        }
+        //redirect add
+        // echo 'Deu ruim';
+        $this->redirect(('/novo'));
+        
     } 
 
 }
--- src/views/pages/add.php
+++ src/views/pages/add.php
@@ -2,13 +2,17 @@
 
 
 <h2>Adicionar um novo usuario</h2>
-<form method="POST" action="<?php $base?>/novo">
+<form method="POST" action="<?= $base?>/novo">
 
-
-
-
-
-
+<label>
+    Nome:<br/>
+    <input type="text" name="name"/>
+</label><br/><br/>
+<label>
+    E-mail:<br/>
+    <input type="email" name="email"/>
+</label>
+    <input type="submit" value="Adicionar"/>
 </form>
 
 
