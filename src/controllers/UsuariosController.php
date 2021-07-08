9756c056d64ae744633a100338179f364a607af0
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-07-08 14:52:50 -0300

    Delete feature  done

--- src/controllers/UsuariosController.php
+++ src/controllers/UsuariosController.php
@@ -64,6 +64,9 @@ class UsuariosController extends Controller {
 
     public function del($args){
         
+        Usuario::delete()->where('id',$args['id'])->execute();  
+        
+        $this->redirect('/');
     }
 
   
